fd5725abc41958314226f38eaf150a0476869700
Bernard Yang <ybernard@gmail.com>
2021-08-16 13:20:24 -0700

    fixed styling

--- src/components/application/Layout.js
+++ src/components/application/Layout.js
@@ -3,7 +3,7 @@ import React from "react";
 import { useHistory, useLocation } from "react-router-dom";
 
 import { useTheme } from "@material-ui/core/styles";
-import { useStyles } from "./LayoutStyle";
+// import { useStyles } from "./LayoutStyle";
 
 import {
   Avatar,
@@ -33,6 +33,103 @@ import { useContext } from "react";
 import { AuthContext } from "context/AuthContext";
 import Landing from "components/landing_page/Landing";
 import { withStyles } from "@material-ui/core/styles";
+import { makeStyles } from "@material-ui/core/styles";
+
+const drawerWidth = 180;
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+  },
+  appBar: {
+    background: "#114B5F",
+
+    zIndex: theme.zIndex.drawer + 2,
+    transition: theme.transitions.create(["width", "margin"], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+  },
+  appBarShift: {
+    marginLeft: drawerWidth,
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(["width", "margin"], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  menuButton: {
+    color: "#FFF",
+    marginRight: 36,
+  },
+  hide: {
+    display: "none",
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+    whiteSpace: "nowrap",
+  },
+  drawerOpen: {
+    // background: "#114B5F",
+    width: drawerWidth,
+    transition: theme.transitions.create("width", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  drawerClose: {
+    // background: "#114B5F",
+    transition: theme.transitions.create("width", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    overflowX: "hidden",
+    width: theme.spacing(7) + 1,
+    [theme.breakpoints.up("sm")]: {
+      width: theme.spacing(9) + 1,
+    },
+  },
+  toolbar: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "flex-end",
+    padding: theme.spacing(0, 1),
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar,
+  },
+  content: {
+    // flexGrow: 1,
+    padding: theme.spacing(3),
+  },
+  active: {
+    color: "#114B5F",
+
+    // padding: "5px",
+    // color: "black",
+  },
+  icons: {
+    marginLeft: "5px",
+  },
+  avatar: { backgroundColor: "#406f7f", color: "#FFF" },
+  app: {
+    flexGrow: 1,
+  },
+  // sidebar: {
+  //   background: "blue",
+  // },
+  btnbtn: {
+    fontWeight: "500",
+    borderRadius: "0px",
+    marginRight: "25px",
+    color: "#FFF",
+    backgroundColor: "#406f7f",
+    "&:hover": {
+      backgroundColor: "#406f7f",
+      boxShadow: "none",
+    },
+  },
+}));
 const options = ["Edit", "Delete"];
 const LightTooltip = withStyles((theme) => ({
   tooltip: {
